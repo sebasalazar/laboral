6bdc745033488e72b9ff7d210bcd241c2ff46419
Sebastian <sebaroms@sebaroms.(none)>
2012-11-17 23:39:31 -0300

    Usuario administrador setiado como '1234567' -rut-, configurado para mysql, el scrit sirve del git sirve para ejecutarlo como mysql

--- web/protected/controllers/UsuariosController.php
+++ web/protected/controllers/UsuariosController.php
@@ -37,7 +37,7 @@ class UsuariosController extends Controller
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
+				'users'=>array('1234567'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
