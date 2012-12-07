e06a2e894d81ce7804f62a8fbfb9a62e819d1fa4
Sebastián Salazar Molina <sebasalazar@gmail.com>
2012-12-06 23:13:12 -0300

    Archivos con datos necesarios

---
+++ SQL/cargar_datos.sql
@@ -0,0 +1,24 @@
+BEGIN TRANSACTION;
+
+-- 
+-- Ingresar datos necesarios Grupo 1
+-- 
+
+
+
+
+
+-- 
+-- Ingresar datos necesarios Grupo OneUp
+-- 
+
+
+
+
+-- 
+-- Ingresar datos necesarios Grupo 3
+-- 
+
+
+
+COMMIT;
