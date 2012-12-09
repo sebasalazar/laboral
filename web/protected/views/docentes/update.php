d5f111b118fc095879320fbf4d806f7a5107e540
Sebastian <sebaroms@sebaroms.(none)>
2012-12-09 17:04:31 -0300

    Cambios menú Docentes

--- web/protected/views/docentes/update.php
+++ web/protected/views/docentes/update.php
@@ -9,6 +9,16 @@ $this->breadcrumbs=array(
 );
 ?>
 
+<?php
+
+$this->menu=array(
+	array('label'=>'Actualizar datos personales', 'url'=>array('update', 'id'=>Yii::app()->user->getModelUsuarioCompleto(Yii::app()->user->name)->pk)),
+	array('label'=>'Publicar Ofertas de Trabajo', 'url'=>array('ofertasLaborales/create')),
+        array('label'=>'Ofertas de Trabajo Publicadas', 'url'=>array('')),
+);
+
+?>
+
 <h1>Actualizar Datos Personales</h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
