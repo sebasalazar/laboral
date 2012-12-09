df2c3e976dba762203289b741a622aa43f02ee46
Sebastian <sebaroms@sebaroms.(none)>
2012-12-09 13:07:45 -0300

    Cambio de Copyright -My Company- a -UTEM-

--- web/protected/views/layouts/main.php
+++ web/protected/views/layouts/main.php
@@ -165,7 +165,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by UTEM.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
