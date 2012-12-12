390c61f1905e5d336a4b6ba8c1e274ca09398b3c
Simón Muñoz Saavedra <sabmus8@gmail.com>
2012-12-11 21:27:11 -0300

    Subiendo datos de carga, modulo de empresa

--- SQL/cargar_datos.sql
+++ SQL/cargar_datos.sql
@@ -11,7 +11,19 @@ BEGIN TRANSACTION;
 -- 
 -- Ingresar datos necesarios Grupo OneUp
 -- 
-
+--Creacion de Empresa
+--
+INSERT INTO regiones (nombre,zona_corfo,codigo,numero) VALUES ('Región de Arica y Parinacota','Norte Grande','XV','15');
+INSERT INTO provincias (nombre,region_fk) VALUES ('Arica',(SELECT pk FROM regiones WHERE nombre='Región de Arica y Parinacota'));
+INSERT INTO comunas (nombre,provincia_fk) VALUES ('Arica',(SELECT pk FROM provincias WHERE nombre='Arica'));
+INSERT INTO rubros (pk,rubro,descripcion) VALUES ('2','Industria','Área industrial');
+--
+--Creación de Practicas
+--
+INSERT INTO jornadas (pk,jornada,descripcion) VALUES ('2','Tarde','De 14.00 a 22.00 Hrs');
+--
+--Fin
+--
 
 
 
