a99f110d3d6d72ecfd1f8cff4d60126f469fecf7
Sebastian <sebaroms@sebaroms.(none)>
2012-12-12 19:06:37 -0300

    Solo admin puede utilizar el CRUD -tips-

--- web/protected/controllers/TipsController.php
+++ web/protected/controllers/TipsController.php
@@ -32,11 +32,11 @@ class TipsController extends Controller
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
+				'actions'=>array(),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
+				'actions'=>array('admin','delete','create','update'),
 				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
