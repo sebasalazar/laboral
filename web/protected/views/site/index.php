19e18895ad354ea35e88bcac906f86f2aa916e41
Sebastián Menéndez <smenendez@icci.cl>
2012-12-13 17:46:02 -0200

    Update web/protected/views/site/index.php
    sin carrusel.

--- web/protected/views/site/index.php
+++ web/protected/views/site/index.php
@@ -1,16 +1,4 @@
-
 <br><br>
-<div class="contenido">
-        <div class="fila">
-            <?php $this->widget('bootstrap.widgets.TbCarousel', array(
-                'items'=>array(
-                    array('image'=>'images/i1.jpeg', 'label'=>'¡Bienvenido!', 'caption'=>'La Bolsa Laboral de la UTEM, está enfocada a facilitar la insercion de nuestros alumnos al mundo laboral, para comenzar ¡Registrate!.'),
-                    array('image'=>'images/i2.jpeg', 'label'=>'¿Por qué inscribirse?', 'caption'=>'Inscribir tu currículo en el Portal de Empleos de tu universidad es el primer paso de tu futuro profesional. ¡Atrevete!'),
-                    array('image'=>'images/i3.jpeg', 'label'=>'Practicas Profesionales', 'caption'=>'¿No tiene donde hacer tu practica?, revisa la sección de Practicas Profesionales, y elige la que más te acomode.'),
-                ),
-            )); ?>
-        </div>
-</div>
 
 <div class="contenido">
         <div class="fila fila-grande">
