bcad4ef1b57c3ef6b91fd2b42056a4e1850444cd
Sebaroms <smenendez@icci.cl>
2012-12-13 13:49:11 -0300

    Cambios diseño

--- web/protected/views/site/index.php
+++ web/protected/views/site/index.php
@@ -19,7 +19,7 @@
                     <text class="text-footer">Noticias</text>
                 </div>
                 <div class="con2">
-                    asddasdasdsa
+                    <p>asddasdasdsa</p>
                 </div>
             </div>
         </div>
@@ -28,14 +28,22 @@
                     <text class="text-footer">Tips</text>
             </div>
             <div class="con3">
-                    asddasdasdsa
+                <MARQUEE behavior="scroll" direction="up" scrollAmount="3" width="420" hiegth="195" onMouseOver="this.scrollAmount=1" onMouseOut="this.scrollAmount=3">
+                    <?php
+                        foreach($model as $i){
+                            echo '<hr class="separador" />';
+                            echo '<b>'.$i->titulo.'</b><br /><br />';
+                            echo '<text class="just">'.$i->contenido.'</text>';
+                            echo '<hr class="separador" />';
+                        }
+                    ?>
+                </MARQUEE>
             </div>
             <br />
             <div class="con">
-                    <text class="text-footer">Tips</text>
+                    <text class="text-footer">Agenda</text>
             </div>
             <div class="con3">
-                    asddasdasdsa
             </div>
         </div>
 </div>
