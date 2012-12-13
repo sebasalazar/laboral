bed79d959acc6ddcc6f01de1c36d242fea7e0f73
Sebaroms <smenendez@icci.cl>
2012-12-13 11:35:21 -0300

    Cambios visuales inicio

--- web/protected/views/site/index.php
+++ web/protected/views/site/index.php
@@ -4,9 +4,9 @@
         <div class="fila">
             <?php $this->widget('bootstrap.widgets.TbCarousel', array(
                 'items'=>array(
-                    array('image'=>'images/i1.jpeg', 'label'=>'First Thumbnail label', 'caption'=>'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'),
-                    array('image'=>'images/i2.jpeg', 'label'=>'Second Thumbnail label', 'caption'=>'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'),
-                    array('image'=>'images/i3.jpeg', 'label'=>'Third Thumbnail label', 'caption'=>'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'),
+                    array('image'=>'images/i1.jpeg', 'label'=>'¡Bienvenido!', 'caption'=>'La Bolsa Laboral de la UTEM, está enfocada a facilitar la insercion de nuestros alumnos al mundo laboral, para comenzar ¡Registrate!.'),
+                    array('image'=>'images/i2.jpeg', 'label'=>'¿Por qué inscribirse?', 'caption'=>'Inscribir tu currículo en el Portal de Empleos de tu universidad es el primer paso de tu futuro profesional. ¡Atrevete!'),
+                    array('image'=>'images/i3.jpeg', 'label'=>'Practicas Profesionales', 'caption'=>'¿No tiene donde hacer tu practica?, revisa la sección de Practicas Profesionales, y elige la que más te acomode.'),
                 ),
             )); ?>
         </div>
