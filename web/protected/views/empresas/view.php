6e07f708a92f0424553ed295d0daa458b57c22a7
Sebaroms <smenendez@icci.cl>
2012-12-15 19:39:38 -0300

    Avance panel administrativo #10

--- web/protected/views/empresas/view.php
+++ web/protected/views/empresas/view.php
@@ -16,21 +16,74 @@ $this->menu=array(
 );
 ?>
 
-<h1>View Empresas #<?php echo $model->pk; ?></h1>
+<div class="menupanel">
+    
+    <?php 
+        $this->widget('bootstrap.widgets.TbMenu', array(
+        'type'=>'tabs', // '', 'tabs', 'pills' (or 'list')
+        'stacked'=>false, // whether this is a stacked menu
+        'items'=>array(
+            array('label'=>'Inicio', 'url'=>array('usuarios/paneladmin')),
+            array('label'=>'Datos Personales', 'items'=>array(
+                                        array('label'=>'Estados Civiles', 'url'=>array('estadosCiviles/admin')),
+                                        array('label'=>'Niveles de Estudios', 'url'=>array('nivelesEstudios/admin')),
+                                        array('label'=>'Regiones', 'url'=>array('regiones/admin')),
+                                        array('label'=>'Provincias', 'url'=>array('provincias/admin')),
+                                        array('label'=>'Comunas', 'url'=>array('comunas/admin')),
+            )),
+            array('label'=>'Universidad', 'items'=>array(
+                                        array('label'=>'Facultades', 'url'=>array('facultades/admin')),
+                                        array('label'=>'Departamentos', 'url'=>array('departamentos/admin')),
+                                        array('label'=>'Carreras', 'url'=>array('carreras/admin')),
+                                        array('label'=>'Docentes', 'url'=>array('docentes/admin')),
+                                        array('label'=>'Estudiantes', 'url'=>array('estudiantes/admin')),
+            )),
+            array('label'=>'Empresa', 'items'=>array(
+                                        array('label'=>'Rubros', 'url'=>array('rubros/admin')),
+                                        array('label'=>'Jornadas', 'url'=>array('jornadas/admin')),
+                                        array('label'=>'Tipos Contratos', 'url'=>array('tiposContratos/admin')),
+                                        array('label'=>'Empresas', 'url'=>array('empresas/admin')),
+                                        array('label'=>'Encargados Empresas', 'url'=>array('encargadosEmpresas/admin')),
+                                        array('label'=>'Encargados Practicas', 'url'=>array('encargadosPracticas/admin')),
+                                        array('label'=>'Evaluaciones Practicas', 'url'=>array('evaluacionesPracticas/admin')),
+            ), 'active'=>true),
+            array('label'=>'Ofertas Laborales', 'items'=>array(
+                                        array('label'=>'Ofertas Laborales', 'url'=>array('ofertasLaborales/admin')),
+                                        array('label'=>'Postulaciones', 'url'=>array('postulaciones/admin')),
+                                        array('label'=>'Practicas', 'url'=>array('practicas/admin')),
+                                        array('label'=>'Propietarios Ofertas Laborales', 'url'=>array('propietarioOferta/admin')),
+            )),
+            
+        ),
+        )); 
+    ?>
+    
+</div>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'pk',
-		'rut',
-		'nombre',
-		'direccion',
-		'comuna_fk',
-		'codigo_postal',
-		'telefono',
-		'email',
-		'actividad_fk',
-		'descripcion_negocio',
-		'web',
-	),
-)); ?>
+<div class="contenidoPage">
+    <h1>Empresa: <?php echo $model->nombre; ?></h1>
+    <br />
+    <?php
+    $this->widget('bootstrap.widgets.TbDetailView', array(
+    'data'=>$model,
+    'attributes'=>array(
+                    'pk',
+                    'rut',
+                    'nombre',
+                    'direccion',
+                    array(
+                        'label'=>'Comunas',
+                        'value'=>$model->comuna->nombre,
+                    ),
+                    'codigo_postal',
+                    'telefono',
+                    'email',
+                    array(
+                        'label'=>'Actividad',
+                        'value'=>$model->actividad->rubro,
+                    ),
+                    'descripcion_negocio',
+                    'web',
+            ),
+    )); ?>
+</div>
