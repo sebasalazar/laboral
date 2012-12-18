6c7449a4f6aa9ccda451a7c8af39ddb276470f7f
Simon <sabmus8@gmail.com>
2012-12-18 01:07:01 -0300

    fix menor en textos

--- web/protected/views/evaluacionesPracticas/update.php
+++ web/protected/views/evaluacionesPracticas/update.php
@@ -7,15 +7,15 @@ $this->breadcrumbs=array(
 	$model->pk=>array('view','id'=>$model->pk),
 	'Update',
 );
-
+/*
 $this->menu=array(
 	array('label'=>'List EvaluacionesPracticas', 'url'=>array('index')),
 	array('label'=>'Create EvaluacionesPracticas', 'url'=>array('create')),
 	array('label'=>'View EvaluacionesPracticas', 'url'=>array('view', 'id'=>$model->pk)),
 	array('label'=>'Manage EvaluacionesPracticas', 'url'=>array('admin')),
-);
+);*/
 ?>
 
-<h1>Update EvaluacionesPracticas <?php echo $model->pk; ?></h1>
+<h1>Modificar Evaluaciones de Practicas <?php echo $model->pk; ?></h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
