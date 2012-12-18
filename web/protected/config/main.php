ab5b1941dcc1059bd1b67718de5434bdbb31a48c
Sebastián Salazar Molina <sebasalazar@gmail.com>
2012-12-18 00:53:09 -0300

    Configuración Switf Mailer

--- web/protected/config/main.php
+++ web/protected/config/main.php
@@ -2,125 +2,115 @@
 
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
-
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'UTEM - Bolsa de Trabajo',
-        'sourceLanguage' => 'es',
-        'language' => 'es',
-	// preloading 'log' component
-	'preload'=>array('bootstrap',),
-	// autoloading model and component classes
-	'import'=>array(
-		'application.models.*',
-		'application.components.*',
-	),
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => 'UTEM - Bolsa de Trabajo',
+    'sourceLanguage' => 'es',
+    'language' => 'es',
+    // preloading 'log' component
+    'preload' => array('bootstrap',),
+    // autoloading model and component classes
+    'import' => array(
+        'application.models.*',
+        'application.components.*',
+    ),
+    'modules' => array(
+        // uncomment the following to enable the Gii tool
 
-	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		
-		'gii'=>array(
-			'class'=>'system.gii.GiiModule',
-			'password'=>'123456',
-			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
-                        'generatorPaths'=>array(
-                            'bootstrap.gii',
-                        ),
-		),
-                'generatorPaths'=>array(
-                    'bootstrap.gii',
-                ),
-		
-	),
+        'gii' => array(
+            'class' => 'system.gii.GiiModule',
+            'password' => '123456',
+            // If removed, Gii defaults to localhost only. Edit carefully to taste.
+            'ipFilters' => array('127.0.0.1', '::1'),
+            'generatorPaths' => array(
+                'bootstrap.gii',
+            ),
+        ),
+        'generatorPaths' => array(
+            'bootstrap.gii',
+        ),
+    ),
+    // application components
+    'components' => array(
+        'user' => array(
+            // enable cookie-based authentication
+            'allowAutoLogin' => true,
+            'class' => 'WebUser',
+        ),
+        //mails mails mails
+        /* 'swiftMailer' => array(
+          'class' => 'ext.swiftMailer.lib.classes.Swift.Mailer',
+          ), */
+        //************************//
+        // uncomment the following to enable URLs in path-format
+        /*
+          'urlManager'=>array(
+          'urlFormat'=>'path',
+          'rules'=>array(
+          '<controller:\w+>/<id:\d+>'=>'<controller>/view',
+          '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+          '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+          ),
+          ),
+         */
+        /*
+          'db'=>array(
+          'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+          ),
+          // uncomment the following to use a MySQL database
+          /*
+          'db'=>array(
+          'connectionString' => 'mysql:host=localhost;dbname=bollaboral',
 
-	// application components
-	'components'=>array(
-		'user'=>array(
-			// enable cookie-based authentication
-			'allowAutoLogin'=>true,
-                        'class'=>'WebUser',
-		),
-            
-            
-            //mails mails mails
-            /*'swiftMailer' => array(
-                'class' => 'ext.swiftMailer.lib.classes.Swift.Mailer',
-            ),*/
-            //************************//
-            
-            
-                'bootstrap'=>array(
-                    'class'=>'ext.bootstrap.components.Bootstrap', // assuming you extracted bootstrap under extensions
-                ),
-		// uncomment the following to enable URLs in path-format
-		/*
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
-		),
-		*/
-            /*
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=bollaboral',
+          /* */
 
-		/**/
-            
-		/*'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=laboral',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '1234',
-			'charset' => 'utf8',
-		),*/
-            
+        /* 'db'=>array(
+          'connectionString' => 'mysql:host=localhost;dbname=laboral',
+          'emulatePrepare' => true,
+          'username' => 'root',
+          'password' => '1234',
+          'charset' => 'utf8',
+          ), */
+
         //Configuracion para postgresql
-         'db'=>array(
-         'connectionString' => 'pgsql:host=localhost;dbname=laboraldb',
-         'username' => 'laboral',
-         'password' => 'laboral',
-         'charset' => 'utf8',
-         ),
-
-		'errorHandler'=>array(
-			// use 'site/error' action to display errors
-			'errorAction'=>'site/error',
-		),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-				// uncomment the following to show log messages on web pages
-				/*
-				array(
-					'class'=>'CWebLogRoute',
-				),
-				*/
-			),
-		),
-            'bootstrap'=>array(
-                'class'=>'ext.bootstrap.components.Bootstrap', // assuming you extracted bootstrap under extensions
+        'db' => array(
+            'connectionString' => 'pgsql:host=localhost;dbname=laboraldb',
+            'username' => 'laboral',
+            'password' => 'laboral',
+            'charset' => 'utf8',
+        ),
+        'errorHandler' => array(
+            // use 'site/error' action to display errors
+            'errorAction' => 'site/error',
+        ),
+        'log' => array(
+            'class' => 'CLogRouter',
+            'routes' => array(
+                array(
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'error, warning',
                 ),
-	),
-
+            // uncomment the following to show log messages on web pages
+            /*
+              array(
+              'class'=>'CWebLogRoute',
+              ),
+             */
+            ),
+        ),
+        'bootstrap' => array(
+            'class' => 'ext.bootstrap.components.Bootstrap', // assuming you extracted bootstrap under extensions
+        ),
         
-	// using Yii::app()->params['paramName']
-	'params'=>array(
-		// this is used in contact page
-		'adminEmail'=>'sabmus8@gmail.com',
-	),
+        'swiftMailer' => array(
+            'class' => 'ext.swiftMailer.SwiftMailer',
+        ),
+    ),
+    // using Yii::app()->params['paramName']
+    'params' => array(
+        // this is used in contact page
+        'adminEmail' => 'sabmus8@gmail.com',
+    ),
 );
