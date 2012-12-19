cf3f3f1dc931c1aa071cd72b310d4abc8a49d639
Sebastián Salazar Molina <ssalazar@orangepeople.cl>
2012-12-19 14:10:42 -0300

    Borro archivos de Backup e innecesarios

--- SQL/cambiostablaestudiantes.sql
+++
@@ -1,8 +0,0 @@
--- Column: al cristian le gusta el completo
-
--- ALTER TABLE estudiantes DROP COLUMN completo;
-
-ALTER TABLE estudiantes ADD COLUMN curriculum_completo boolean;
-ALTER TABLE estudiantes ALTER COLUMN curriculum_completo SET NOT NULL;
-ALTER TABLE estudiantes ALTER COLUMN curriculum_completo SET DEFAULT false;
-
