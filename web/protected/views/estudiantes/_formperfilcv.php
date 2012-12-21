4d41f906912e484169a16e36a5be7c175ee3aa0b
Cristobal Jaramillo <carjaram@gmail.com>
2012-12-21 02:35:12 -0300

    cambios

--- web/protected/views/estudiantes/_formperfilcv.php
+++ web/protected/views/estudiantes/_formperfilcv.php
@@ -22,10 +22,11 @@
 		<?php echo $form->error(Estudiantes::model(),'archivo_curriculum'); ?>
 	</div>
   
-
+         <?php   CHtml::link(CHtml::encode($model->archivo_curriculum), Yii::app()->baseUrl . '/cv/' . $model->archivo_curriculum); ?>
 	<div class="row buttons">
 		 <?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit', 'type'=>'primary', 'label'=>'Enviar')); ?>
             <?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'reset', 'label'=>'Borrar')); ?>
+
 	</div>
 
 
